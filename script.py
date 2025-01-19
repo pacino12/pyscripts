78766506da8c9b715b21257b951e7ded43dc75c0
pacino <typaccy@gmail.com>
2025-01-19 02:53:34 +0100

    Python script

---
+++ script.py
@@ -0,0 +1,36 @@
+"""
+What is the script is generally for :
+1.to add user
+2.Add group
+3.Add user to group
+4.Create directory
+5.Assign user & group ownership to directory
+6.Test if user or dir exists , if not create it
+7.SSH in python
+8.Fabric library
+9.webserver provisioning
+10.python virtual environment
+11.Python for other scripting tasks
+
+!! This can easily be done using ainsible the automation tool
+"""
+import os
+
+userlist = ["alpha", "beta", "gamma"]
+
+print("Adding users to system ")
+print("############################################")
+
+
+# Loop to add user from userlist
+for user in userlist:
+    exitcode = os.system("id {}".format(user))
+    if exitcode != 0:
+        print("User does not exist. Adding it." .fomat(user))
+        print("############################################")
+        print()
+        os.system("useradd {} ".format(user))
+    else:
+        print("User already exist , skipping it.")
+        print("##################################")
+        print()
